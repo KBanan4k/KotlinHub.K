22628f5a643669c9cd49ea754d5162b8d3a63744
ITStepJunior <brestitstep3@gmail.com>
2024-10-31 11:42:21 +0300

    add new function:buttonSubmit_edit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,8 @@
 package org.example
 
 fun main() {
-    val name = "Kotlin"
-
-    println("Hello, " + name + "!")
-    idea()
     buttonSubmit()
     }
-fun idea(){
-    println("New data")
-}
 fun buttonSubmit(){
     println("Ваши данные успешно отправлены")
 }
